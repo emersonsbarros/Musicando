2ad708c53bf197e5f9efe57a1f62939442b12d06
Emerson Barros <emerson.sbarros@gmail.com>
2014-07-06 03:08:00 -0300

    add

--- Musicando/Biblioteca.m
+++ Musicando/Biblioteca.m
@@ -164,8 +164,8 @@
     Aula *mod2Aula2 = [[Aula alloc]init];
     mod2Aula2.nome = @"Pausas";
     mod2Aula2.capa = [UIImage imageNamed:@"aula.png"];
-    [[mod2Aula1 listaDeExercicios]addObject: mod2Aula2Exe1];
-    [[mod2Aula1 listaDeExercicios]addObject: mod2Aula2Exe2];
+    [[mod2Aula2 listaDeExercicios]addObject: mod2Aula2Exe1];
+    [[mod2Aula2 listaDeExercicios]addObject: mod2Aula2Exe2];
     
     
 ////////////////////////////////// Aula 3 //////////////////////////////////////
@@ -179,24 +179,113 @@
     Aula *mod2Aula3 = [[Aula alloc]init];
     mod2Aula3.nome = @"Pentagrama";
     mod2Aula3.capa = [UIImage imageNamed:@"aula.png"];
-    [[mod2Aula1 listaDeExercicios]addObject: mod2Aula3Exe1];
-    [[mod2Aula1 listaDeExercicios]addObject: mod2Aula3Exe2];
-    [[mod2Aula1 listaDeExercicios]addObject: mod2Aula3Exe3];
+    [[mod2Aula3 listaDeExercicios]addObject: mod2Aula3Exe1];
+    [[mod2Aula3 listaDeExercicios]addObject: mod2Aula3Exe2];
+    [[mod2Aula3 listaDeExercicios]addObject: mod2Aula3Exe3];
 
+
+////////////////////////////////// Aula 4 //////////////////////////////////////
+    
+//Exercicios
+    Exercicio *mod2Aula4Exe1 = [[Exercicio alloc] init:201 nome:@"Introdução a simbologia das notas" nomeView:@"Mod2Aula1Exe1" experiencia:10 pontuacaoMaxima:10 mascote:self.mascote :[UIImage imageNamed:@"exercicios.png"]];
+    Exercicio *mod2Aula4Exe2 = [[Exercicio alloc] init:202 nome:@"Apresentando a Imagem das notas" nomeView:@"Mod2Aula1Exe2" experiencia:10 pontuacaoMaxima:10 mascote:self.mascote :[UIImage imageNamed:@"exercicios.png"]];
+    Exercicio *mod2Aula4Exe3 = [[Exercicio alloc] init:202 nome:@"Treinando com Imagem das notas" nomeView:@"Mod2Aula1Exe2" experiencia:10 pontuacaoMaxima:10 mascote:self.mascote :[UIImage imageNamed:@"exercicios.png"]];
+    Exercicio *mod2Aula4Exe4 = [[Exercicio alloc] init:202 nome:@"Treinamento" nomeView:@"Mod2Aula1Exe2" experiencia:10 pontuacaoMaxima:10 mascote:self.mascote :[UIImage imageNamed:@"exercicios.png"]];
+    Exercicio *mod2Aula4Exe5 = [[Exercicio alloc] init:202 nome:@"Novas imagens de Notas" nomeView:@"Mod2Aula1Exe2" experiencia:10 pontuacaoMaxima:10 mascote:self.mascote :[UIImage imageNamed:@"exercicios.png"]];
+    Exercicio *mod2Aula4Exe6 = [[Exercicio alloc] init:202 nome:@"Introdução aos símbolos das pausas" nomeView:@"Mod2Aula1Exe2" experiencia:10 pontuacaoMaxima:10 mascote:self.mascote :[UIImage imageNamed:@"exercicios.png"]];
+    Exercicio *mod2Aula4Exe7 = [[Exercicio alloc] init:202 nome:@"Resumo final" nomeView:@"Mod2Aula1Exe2" experiencia:10 pontuacaoMaxima:10 mascote:self.mascote :[UIImage imageNamed:@"exercicios.png"]];
+
+    
+//Aula de Notação nota/pausa
+    Aula *mod2Aula4 = [[Aula alloc]init];
+    mod2Aula4.nome = @"Notação nota/pausa";
+    mod2Aula4.capa = [UIImage imageNamed:@"aula.png"];
+    [[mod2Aula4 listaDeExercicios]addObject: mod2Aula4Exe1];
+    [[mod2Aula4 listaDeExercicios]addObject: mod2Aula4Exe2];
+    [[mod2Aula4 listaDeExercicios]addObject: mod2Aula4Exe3];
+    [[mod2Aula4 listaDeExercicios]addObject: mod2Aula4Exe4];
+    [[mod2Aula4 listaDeExercicios]addObject: mod2Aula4Exe5];
+    [[mod2Aula4 listaDeExercicios]addObject: mod2Aula4Exe6];
+    [[mod2Aula4 listaDeExercicios]addObject: mod2Aula4Exe7];
+
+    
+////////////////////////////////// Aula 5 //////////////////////////////////////
+    
+//Exercicios
+    Exercicio *mod2Aula5Exe1 = [[Exercicio alloc] init:201 nome:@"Introdução às Oitavas" nomeView:@"Mod2Aula1Exe1" experiencia:10 pontuacaoMaxima:10 mascote:self.mascote :[UIImage imageNamed:@"exercicios.png"]];
+    
+//Aula de Oitavas
+    Aula *mod2Aula5 = [[Aula alloc]init];
+    mod2Aula5.nome = @"Oitavas";
+    mod2Aula5.capa = [UIImage imageNamed:@"aula.png"];
+    [[mod2Aula5 listaDeExercicios]addObject: mod2Aula5Exe1];
+    
+    
+////////////////////////////////// Aula 6 //////////////////////////////////////
+    
+//Exercicios
+    Exercicio *mod2Aula6Exe1 = [[Exercicio alloc] init:201 nome:@"Introdução às Claves" nomeView:@"Mod2Aula1Exe1" experiencia:10 pontuacaoMaxima:10 mascote:self.mascote :[UIImage imageNamed:@"exercicios.png"]];
+    
+//Aula de Claves
+    Aula *mod2Aula6 = [[Aula alloc]init];
+    mod2Aula6.nome = @"Claves";
+    mod2Aula6.capa = [UIImage imageNamed:@"aula.png"];
+    [[mod2Aula6 listaDeExercicios]addObject: mod2Aula6Exe1];
+    
+    
+    
+////////////////////////////////// Aula 7 //////////////////////////////////////
+    
+//Exercicios
+    Exercicio *mod2Aula7Exe1 = [[Exercicio alloc] init:201 nome:@"Introdução ao Compasso" nomeView:@"Mod2Aula1Exe1" experiencia:10 pontuacaoMaxima:10 mascote:self.mascote :[UIImage imageNamed:@"exercicios.png"]];
+    Exercicio *mod2Aula7Exe2 = [[Exercicio alloc] init:201 nome:@"Número de Compasso" nomeView:@"Mod2Aula1Exe1" experiencia:10 pontuacaoMaxima:10 mascote:self.mascote :[UIImage imageNamed:@"exercicios.png"]];
+    Exercicio *mod2Aula7Exe3 = [[Exercicio alloc] init:201 nome:@"Unidade de Tempo" nomeView:@"Mod2Aula1Exe1" experiencia:10 pontuacaoMaxima:10 mascote:self.mascote :[UIImage imageNamed:@"exercicios.png"]];
+    Exercicio *mod2Aula7Exe4 = [[Exercicio alloc] init:201 nome:@"Fórmula de Compasso" nomeView:@"Mod2Aula1Exe1" experiencia:10 pontuacaoMaxima:10 mascote:self.mascote :[UIImage imageNamed:@"exercicios.png"]];
+    Exercicio *mod2Aula7Exe5 = [[Exercicio alloc] init:201 nome:@"Complemento" nomeView:@"Mod2Aula1Exe1" experiencia:10 pontuacaoMaxima:10 mascote:self.mascote :[UIImage imageNamed:@"exercicios.png"]];
+    Exercicio *mod2Aula7Exe6 = [[Exercicio alloc] init:201 nome:@"Compasso Ggestual" nomeView:@"Mod2Aula1Exe1" experiencia:10 pontuacaoMaxima:10 mascote:self.mascote :[UIImage imageNamed:@"exercicios.png"]];
+    
+//Aula de Compasso
+    Aula *mod2Aula7 = [[Aula alloc]init];
+    mod2Aula7.nome = @"Compasso";
+    mod2Aula7.capa = [UIImage imageNamed:@"aula.png"];
+    [[mod2Aula7 listaDeExercicios]addObject: mod2Aula7Exe1];
+    [[mod2Aula7 listaDeExercicios]addObject: mod2Aula7Exe2];
+    [[mod2Aula7 listaDeExercicios]addObject: mod2Aula7Exe3];
+    [[mod2Aula7 listaDeExercicios]addObject: mod2Aula7Exe4];
+    [[mod2Aula7 listaDeExercicios]addObject: mod2Aula7Exe5];
+    [[mod2Aula7 listaDeExercicios]addObject: mod2Aula7Exe6];
+    
+    
+    
+////////////////////////////////// Aula 8 //////////////////////////////////////
+    
+//Exercicios
+    Exercicio *mod2Aula8Exe1 = [[Exercicio alloc] init:201 nome:@"Resumo de Partitura" nomeView:@"Mod2Aula1Exe1" experiencia:10 pontuacaoMaxima:10 mascote:self.mascote :[UIImage imageNamed:@"exercicios.png"]];
+    
+//Aula de Claves
+    Aula *mod2Aula8 = [[Aula alloc]init];
+    mod2Aula8.nome = @"Claves";
+    mod2Aula8.capa = [UIImage imageNamed:@"aula.png"];
+    [[mod2Aula8 listaDeExercicios]addObject: mod2Aula8Exe1];
     
     
 ///////////////////////////// Modulo Intermediario //////////////////////////////////
     
     //Cria o modulo Intermediario
-    Modulo *mod = [[Modulo alloc]init];
-    mod.nome = @"Intermediario";
-    mod.capa = [UIImage imageNamed:@"modulo.png"];
-    [[mod listaDeAulas]addObject: mod2Aula1];
-    [[mod listaDeAulas]addObject: mod2Aula2];
-    
+    Modulo *moduloIntermediario = [[Modulo alloc]init];
+    moduloIntermediario.nome = @"Intermediario";
+    moduloIntermediario.capa = [UIImage imageNamed:@"modulo.png"];
+    [[moduloIntermediario listaDeAulas]addObject: mod2Aula1];
+    [[moduloIntermediario listaDeAulas]addObject: mod2Aula2];
+    [[moduloIntermediario listaDeAulas]addObject: mod2Aula3];
+    [[moduloIntermediario listaDeAulas]addObject: mod2Aula4];
+    [[moduloIntermediario listaDeAulas]addObject: mod2Aula5];
+    [[moduloIntermediario listaDeAulas]addObject: mod2Aula6];
+    [[moduloIntermediario listaDeAulas]addObject: mod2Aula7];
+    [[moduloIntermediario listaDeAulas]addObject: mod2Aula8];
     
     //Adiciona o Modulo dentro da biblioteca
-    [[self listaDeModulos]addObject:mod];
+    [[self listaDeModulos]addObject: moduloIntermediario];
     
 }
 
